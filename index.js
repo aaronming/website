e3b400d4aaf4d4aff57d56504917201d5545e634
Aaron Ming <aaronming11@gmail.com>
2012-04-27 17:25:35 -0700

    NEW LAYOUT

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	var state = 1;
+	
+	$("#contentAboutMe").hide();
+	$("#contentDrawings").hide();
+	$("#contentProjects").hide();
+	
+	
 	$(".aLink").hover(
 		function(){
 			$(this).css("color", "darkgrey")
@@ -7,5 +14,34 @@ $(document).ready(function(){
 			$(this).css("color", "grey")
 		}
 	);
+	
+	$("#linkHome").click(function(){
+		if (state != 1){
+			$(".contentClass").fadeOut();
+			$("#contentHome").delay(400).fadeToggle();
+			state = 1;
+		}
+	});
+	$("#linkAboutMe").click(function(){
+		if (state != 2){
+			$(".contentClass").fadeOut();
+			$("#contentAboutMe").delay(400).fadeToggle();
+			state = 2;
+		}
+	});
+	$("#linkProjects").click(function(){
+		if (state != 3){
+			$(".contentClass").fadeOut();
+			$("#contentProjects").delay(400).fadeToggle();
+			state = 3;
+		}
+	});
+	$("#linkDrawings").click(function(){
+		if (state != 4){
+			$(".contentClass").fadeOut();
+			$("#contentDrawings").delay(400).fadeToggle();
+			state = 4;
+		}
+	});
 
 });
